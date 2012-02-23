acda2bc4b121aac62458fa94038f05ceef3424f9
Nick Meyer <nicholasmeyer@gmail.com>
2012-02-23 00:32:31 -0500

    Made kill error silent

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ -f twistd.pid ]
 then
-    sudo kill -9 $(sudo cat twistd.pid)
+    sudo kill -9 $(sudo cat twistd.pid) 2>/dev/null
     sudo twistd -y pranky.py
 else
     sudo twistd -y pranky.py
